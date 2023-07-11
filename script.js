1efac87ecdc45b9e920a9980fc3dce95c42ed29f
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-11 09:42:56 -0300

    agregue la lista de tareas

---
+++ script.js
@@ -0,0 +1,17 @@
+const inputNuevaTarea = document.querySelector("#nueva-tarea");
+const CLAVE_TAREAS = "tarea";
+
+let listaTareas = "lista vacia";
+listaTareas = [];
+
+function agregar() {
+    let nuevaTarea = inputNuevaTarea.value;
+    document.querySelector("ol").innerHTML += `
+        <li> ${nuevaTarea} </li>
+    `;
+
+    
+    listaTareas.push(nuevaTarea);
+
+    localStorage.setItem(CLAVE_TAREAS,JSON.stringify(listaTareas) );
+}
