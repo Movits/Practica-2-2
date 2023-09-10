08782b5093477d46c6ed46ba140d84c0046e4499
Roberto Alagemovits <robertomovits@gmail.com>
2023-09-10 01:45:58 -0300

    modularize la funcion buscar con el resuelto y agregue funcion actualizar

--- script.js
+++ script.js
@@ -1,54 +1,61 @@
 const inputNuevaTarea = document.querySelector("#nueva-tarea");
 const buscarTarea = document.querySelector("#buscar-tarea");
 const divTareaBuscada = document.querySelector("#tarea-buscada");
-const CLAVE_TAREAS = "tarea";
+const inputActualizarTareaTxt = document.querySelector("#actualizar-tarea-txt");
+const inputActualizarTareaNum = document.querySelector("#actualizar-tarea-num");
+
+const CLAVE_TAREAS = "listaTareas";
 
 let listaTareas = "lista vacia";
 listaTareas = [];
 
 function iniciarListaTareas() {
-    listaTareas = JSON.parse(localStorage.getItem(CLAVE_TAREAS));
-    if (listaTareas === null) {
-        listaTareas = [];
-        localStorage.setItem(CLAVE_TAREAS, JSON.stringify(listaTareas));
-    }
+  listaTareas = JSON.parse(localStorage.getItem(CLAVE_TAREAS));
+  if (listaTareas === null) {
+    listaTareas = [];
+    actualizarLocalStorage();
+  }
 
-    listaTareas.forEach(tarea => {
+  generarListaTareas();
+}
+
+function actualizarLocalStorage() {
+    localStorage.setItem(CLAVE_TAREAS, JSON.stringify(listaTareas));
+}
+
+function generarListaTareas() {
+    document.querySelector("ol").innerHTML = "";
+    listaTareas.forEach((tarea) => {
         document.querySelector("ol").innerHTML += `
             <li> ${tarea} </li>
-        `; 
+        `;
     });
 }
 
 function agregar() {
-    let nuevaTarea = inputNuevaTarea.value;
-    document.querySelector("ol").innerHTML += `
+  let nuevaTarea = inputNuevaTarea.value;
+  document.querySelector("ol").innerHTML += `
         <li> ${nuevaTarea} </li>
     `;
 
-    
-    listaTareas.push(nuevaTarea);
-
-    localStorage.setItem(CLAVE_TAREAS,JSON.stringify(listaTareas) );
+  listaTareas.push(nuevaTarea);
+  actualizarLocalStorage();
 }
 
 function buscar() {
-    let found = false;
-
-    for (let i = 0; i < listaTareas.length && !found; i++) {
-        if (i + 1 === parseInt(buscarTarea.value)) {
-            divTareaBuscada.innerHTML = `<span>Tarea buscada: ${listaTareas[i]}</span>`;
-            found = true;
-        }
-    }
-
-    if (!found) {
-        divTareaBuscada.innerHTML = `<span>Tarea no encontrada</span>`;
-    }
+    divTareaBuscada.innerHTML = `
+        <span>Tarea buscada: ${listaTareas[buscarTarea.value - 1]}</span>
+    `;
 }
 
 function actualizar() {
-    
+  let posTarea = inputActualizarTareaNum.value;
+  let txtNuevaTarea = inputActualizarTareaTxt.value;
+
+  listaTareas[posTarea - 1] = txtNuevaTarea;
+
+  generarListaTareas();
+  actualizarLocalStorage();
 }
 
 /* localStorage.clear() */
