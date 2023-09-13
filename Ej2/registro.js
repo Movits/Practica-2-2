425fe510092e586c4e83144f6586402ae78f8f3f
Roberto Alagemovits <robertomovits@gmail.com>
2023-09-13 18:08:43 -0300

    agregue una manera ed verificar si el usuario ya existe

--- Ej2/registro.js
+++ Ej2/registro.js
@@ -1,32 +1,45 @@
 function guardarInformacion() {
     const entradaEmail = document.getElementById("email");
-    const inputContrasena = document.getElementById("password");
-    const inputContrasenaConfirmacion = document.getElementById("passwordConfirm");
+    const entradaContrasena = document.getElementById("password");
+    const entradaContrasenaConfirmacion = document.getElementById("passwordConfirm");
     const entradaNombre = document.getElementById("nombre");
     const entradaApellido1 = document.getElementById("apellido1");
     const entradaApellido2 = document.getElementById("apellido2");
     const entradaGenero = document.querySelector("input[name='gender']:checked").value;
     
-    if (inputContrasena.value !== inputContrasenaConfirmacion.value) {
+    if (entradaContrasena.value !== entradaContrasenaConfirmacion.value) {
         alert('Las contraseñas no coinciden.');
         return false; // No continuar si las contraseñas no coinciden
     }
 
     const informacionUsuario = {
         email: entradaEmail.value,
-        contrasena: inputContrasena.value,
+        contrasena: entradaContrasena.value,
         nombre: entradaNombre.value,
         apellido1: entradaApellido1.value,
         apellido2: entradaApellido2.value,
         genero: entradaGenero
     };
-
-    // Obtiene la lista actual de usuarios
+    
     const usuariosActuales = JSON.parse(localStorage.getItem('listaUsuarios') || '[]');
-    // Agrega el nuevo usuario
-    usuariosActuales.push(informacionUsuario);
-    // Guarda la lista de nuevo en localStorage
-    localStorage.setItem('listaUsuarios', JSON.stringify(usuariosActuales));
+    let usuarioYaExiste = false;
 
+    for (let i = 0; i < usuariosActuales.length; i++) {
+        if (usuariosActuales[i].email === informacionUsuario.email) {
+            usuarioYaExiste = true;
+            break; // Exit the loop once a matching email is found
+        }
+    }
+
+    if (!usuarioYaExiste) {
+        // Agrega el nuevo usuario
+        usuariosActuales.push(informacionUsuario);
+        // Guarda la lista de nuevo en localStorage
+        localStorage.setItem('listaUsuarios', JSON.stringify(usuariosActuales));
+    } else {
+        alert('El usuario ya existe.');
+    
+    }
+    
     window.location.href = "index.html";
 }
